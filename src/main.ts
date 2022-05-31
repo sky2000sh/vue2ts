ace866eedb90c265acb3321235ed35b6e4112eaf
sky2000sh <sky2000sh@gmail.com>
2022-05-31 23:33:14 +0900

    commit on 31/05/22

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import firebase from './plugins/firebase'
 
 Vue.config.productionTip = false
 
